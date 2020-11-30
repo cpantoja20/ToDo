6b0f83879203dab76bd5b3a12dded6a70ba4eca0
eliza2692 <eliza2692@hotmail.com>
2020-11-29 22:09:48 -0500

    Define test and set up try case

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@
 test: 
 	tox	-e venv
 
+.PHONY: run
+run: 
+	python todo_app.py
+
 .PHONY: clean
 clean:
 	find . -type d -name __pycache__ -exec rm -r {} \+
--- Test/Todo_app_test.py
+++ Test/Todo_app_test.py
@@ -1,1 +1,8 @@
 #Test/Todo_app_test.py
+
+import pytest
+from todo_app import app
+
+def test_index():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
