af6514359c262f062a49dcc6ee60721decd652cb
eliza2692 <eliza2692@hotmail.com>
2020-12-30 15:50:44 -0500

    React Comment Approval

--- Todo_app/Client/todo/src/index.js
+++ Todo_app/Client/todo/src/index.js
@@ -1,10 +1,39 @@
 //import React and reactDom libraries
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from 'faker';
+// Components
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 //Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Nena" 
+                    timeAgo="Today at 4:45AM"
+                    content="Nice work"
+                    avatar={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Cyndy" 
+                    timeAgo="Today at 3:15PM"
+                    content="You're the best"
+                    avatar={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Ivon" 
+                    timeAgo="Yesterday at 5:24AM"
+                    content="Do not give up"
+                    avatar={faker.image.image()}/>
+            </ApprovalCard>
+        </div>
+    );
 };
 
 //show react component in the screen
