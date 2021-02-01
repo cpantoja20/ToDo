3c5f091a91b050f3e8e57690fc1545cacceda5a1
eliza2692 <eliza2692@hotmail.com>
2021-02-01 14:48:07 -0500

    React navigation

--- Todo_app/Client/widget/src/App.js
+++ Todo_app/Client/widget/src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -19,12 +21,45 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'Rojo',
+        value: 'red'
+    },
+    {
+        label: 'Verde',
+        value: 'green'
+    },
+    {
+        label: 'Azul',
+        value: 'blue'
+    }
+];
 
 export default () => {
 
+    const [selected, setSelected] = useState(options[0])
+
     return (
         <div>
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="dropdown">
+                <Dropdown 
+                    label="Select a Color"
+                    selected={selected} 
+                    onSelectedChange={setSelected} 
+                    options={options}  
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
