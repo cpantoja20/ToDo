514d4cc70d88aaa9b146f7df6311b368ede831fc
eliza2692 <eliza2692@hotmail.com>
2021-02-09 16:09:31 -0500

    Delete Trah

--- Todo_app/Client/Try1/todo/src/Api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization:'Client-ID jgXEopIcHGu7pLjVQkE5F6KSGNzYr0spV7dk0uFpgHk'
-    }
-});
--- Todo_app/Client/Try1/todo/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom, { render } from 'react-dom';
-import App from './components/App';
-
-
-//show react component in the screen
-ReactDom.render(<App/>, document.querySelector('#root'));
