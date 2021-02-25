c88807033d1145ff3c19883041a04cbe23c0c3ca
eliza2692 <eliza2692@hotmail.com>
2021-02-25 16:05:03 -0500

    Conection Api with Client

--- todo_app.py
+++ todo_app.py
@@ -1,11 +1,12 @@
 #todo_app.py
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api',methods=['GET'])
+def index():
+    return {
+        'name':'Hello Word'
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
