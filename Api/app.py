0ff0073b86b3e11e3e2e47f7e67ae1ed3419eb57
eliza2692 <eliza2692@hotmail.com>
2021-03-16 18:46:54 -0500

    Conect to database and fetch data

--- Api/app.py
+++ Api/app.py
@@ -1,48 +1,35 @@
 # Api/app.py
-
 from flask import Flask, jsonify, request,json
 from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///example.db"
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-
-    def __str__(self):
-        return f'{self.id} {self.name} {self.content}'
-
-def todo_serializer(todo):
+def todo_serializer(task):
     return {
-        'id': todo.id,
-        'name': todo.name,
-        'content': todo.content
+        'id': task[0],
+        'name': task[1],
+        'content': task[2]
     }   
 
 @app.route('/api', methods = ['GET'])
 def index():
-    return jsonify([*map(todo_serializer,Todo.query.all())])
+    return jsonify([*map(todo_serializer,db.select_all_task())])
 
 @app.route('/api/create', methods = ['POST'])
 def create():
     request_data = json.loads(request.data)
-    todo = Todo(name = request_data['name'], content = request_data['content'])
-    db.session.add(todo)
-    db.session.commit()
+    db.insert_task(request_data['name'], request_data['content'])
     return{'201':'To do created succesfully'}
 
 @app.route('/api/<int:id>')
 def show(id):
-    return jsonify([*map(todo_serializer,Todo.query.filter_by(id=id))])
+    return jsonify([*map(todo_serializer,db.select_task(id))])
 
 @app.route('/api/<int:id>', methods = ['POST'])
 def delete(id):
     request_data = json.loads(request.data)
-    Todo.query.filter_by(id=request_data['id']).delete()
-    db.session.commit()
+    db.delete_task(id)
     return {'204':'To do deleted succesfully'}
 
 if __name__ == '__main__':
---
+++ Api/db/db.py
@@ -0,0 +1,76 @@
+#Todo_app/Api/db/__init__.py
+
+import psycopg2
+
+db_config = {'database': 'todo', 'user': 'postgres',
+                     'password': 'Nenadb123', 'host': '127.0.0.1', 'port': '5432'}
+
+while True:
+    try:
+        conn = psycopg2.connect(**db_config)
+        break
+    except:
+        conn = psycopg2.connect(
+            database="postgres", 
+            user='postgres', 
+            password='Nenadb123', 
+            host='127.0.0.1', 
+            port= '5432'
+            )
+        conn.autocommit = True
+        cursor = conn.cursor()
+        #Creating a database
+        cursor.execute('''CREATE database ToDo''')
+        conn.close()
+
+def create_tables():
+    conn = psycopg2.connect(**db_config)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Tasks(
+        id serial NOT NULL PRIMARY KEY,
+        name CHAR(20) NOT NULL,
+        description VARCHAR(255) NOT NULL
+        )''')
+    cursor.close()
+    conn.close()
+
+# task
+def insert_task(name, description):
+    conn = psycopg2.connect(**db_config)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO Tasks (id, name, description) VALUES (DEFAULT,%s,%s)''', (name, description))
+    cursor.close()
+    conn.close()
+
+def delete_task(taskId):
+    conn = psycopg2.connect(**db_config)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM Tasks WHERE id = %s""", (str(taskId)))
+    cursor.close()
+    conn.close()
+
+def select_task(taskId):
+    conn = psycopg2.connect(**db_config)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM Tasks WHERE id = %s""", (str(taskId)))
+    task = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return task
+
+def select_all_task():
+    conn = psycopg2.connect(**db_config)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM Tasks""")
+    tasks = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return tasks
+
+create_tables()
+
